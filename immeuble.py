610540272b8db5d599a79da1564862ff2e4aa622
ikzen <flo.bodin@orange.fr>
2026-09-11 09:41:55 +0200

    porte

--- immeuble.py
+++ immeuble.py
@@ -1,2 +1,17 @@
 from turtle import *
- bla bla bla
+
+setup(500, 500)
+
+def porte():
+    up()
+    goto(-200, -100)
+    down()
+    pensize(2)
+    left(90)
+    for i in range(2):
+        forward(60)
+        right(90)
+        forward(40)
+        right(90)
+
+print(porte())
