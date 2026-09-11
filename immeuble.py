df63a9cd1db41651c75705eabdeff81922b226e2
ikzen <flo.bodin@orange.fr>
2026-09-11 09:53:38 +0200

    couleur

--- immeuble.py
+++ immeuble.py
@@ -1,15 +1,35 @@
 from turtle import *
+from random import rd 
 
 setup(500, 500)
 
 def porte():
+
+    """immeuble"""
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+    """porte"""
     up()
     goto(-200, -100)
+    begin_fill()
+    fillcolor(rd.choice(["brown", "black", "gray"]))
     down()
     goto(-200, -50)
     goto(-170, -50)
     goto(-170, -100)
     goto(-200, -100)
+    end_fill()
 
 
 print(porte())
