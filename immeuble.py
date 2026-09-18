4b7ba832ab38fb903702c0520e0a06271225a033
ikzen <flo.bodin@orange.fr>
2026-09-18 09:12:21 +0200

    fenetre

--- immeuble.py
+++ immeuble.py
@@ -3,52 +3,115 @@ from random import *
 
 setup(1000, 500)
 colormode(255)
+speed(1)
 
 def projet():
 
     
     """maison 1"""
 
+
+    # position et couleur
     up()
-    goto(-175, -100)
-    color = (randint(0,255),randint(0,255),randint(0,255))
-    pencolor(color)
-    fillcolor(color)
-    down()
-    begin_fill()
-    forward(140)
-    left(90)
-    forward(60)
-    left(90)      
-    forward(140)
-    left(90)
-    forward(60)
-    end_fill()
-
-
-
-    """maison 2"""
-    
-    up()
-    left(90)
     goto(-355, -100)
     color = (randint(0,255),randint(0,255),randint(0,255))
     pencolor(color)
     fillcolor(color)
     down()
+
+
+    #taille de l'immeuble
+    etage = randint(1,5)
+    if etage == 1:
+        nb = 60
+    elif etage == 2:
+        nb = 120
+    elif etage == 3:
+        nb = 180
+    elif etage == 4:
+        nb = 240
+    else:
+        nb = 300
+
+
+    #remplissage
     begin_fill()
     forward(140)
     left(90)
-    forward(60)
+    forward(nb)
     left(90)      
     forward(140)
     left(90)
-    forward(60)
+    forward(nb)
+    end_fill()
+
+
+    #fenetre
+
+    i = 0
+    up()
+    goto(-340, -60)
+    down()
+    fillcolor(255, 255, 255)
+    pencolor(0, 0, 0)
+    begin_fill()
+    for j in range(0,4):
+        while i != 4 :
+            forward(30)
+            left(90)
+            i += 1
+        up()
+        goto(-340, -60)
+        forward(40)   
+    end_fill()
+
+
+
+
+
+    """maison 2"""
+
+
+    # position et couleur
+    up()
+    left(90)
+    goto(-175, -100)
+    color = (randint(0,255),randint(0,255),randint(0,255))
+    pencolor(color)
+    fillcolor(color)
+    down()
+
+
+    #taille de l'immeuble
+    etage = randint(1,5)
+    if etage == 1:
+        nb = 60
+    elif etage == 2:
+        nb = 120
+    elif etage == 3:
+            nb = 180
+    elif etage == 4:
+        nb = 240
+    else:
+        nb = 300
+
+
+    #remplissage
+    begin_fill()
+    forward(140)
+    left(90)
+    forward(nb)
+    left(90)      
+    forward(140)
+    left(90)
+    forward(nb)
     end_fill()
 
 
     """maison 3"""
-    
+
+
+    # position et couleur
     up()
     left(90)
     goto(5, -100)
@@ -56,19 +119,37 @@ def projet():
     pencolor(color)
     fillcolor(color)
     down()
+
+
+    #taille de l'immeuble
+    etage = randint(1,5)
+    if etage == 1:
+        nb = 60
+    elif etage == 2:
+        nb = 120
+    elif etage == 3:
+        nb = 180
+    elif etage == 4:
+        nb = 240
+    else:
+        nb = 300
+
+
+    #remplissage
     begin_fill()
     forward(140)
     left(90)
-    forward(60)
+    forward(nb)
     left(90)      
     forward(140)
     left(90)
-    forward(60)
+    forward(nb)
     end_fill()
 
 
     """maison 4"""
-    
+
+    # position et couleur
     up()
     left(90)
     goto(185, -100)
@@ -76,14 +157,31 @@ def projet():
     pencolor(color)
     fillcolor(color)
     down()
+
+
+    #taille de l'immeuble
+    etage = randint(1,5)
+    if etage == 1:
+        nb = 60
+    elif etage == 2:
+        nb = 120
+    elif etage == 3:
+        nb = 180
+    elif etage == 4:
+        nb = 240
+    else:
+        nb = 300
+
+        
+    #remplissage
     begin_fill()
     forward(140)
     left(90)
-    forward(60)
+    forward(nb)
     left(90)      
     forward(140)
     left(90)
-    forward(60)
+    forward(nb)
     end_fill()
 
 
